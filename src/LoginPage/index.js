b6eb1979c43184cc1b9a46ddaa301eff24076bf7
손동찬 <charliesohn@sondongchan-ui-MacBookAir.local>
2022-02-24 10:06:59 +0900

    submitting

--- src/LoginPage/index.js
+++ src/LoginPage/index.js
@@ -1,10 +1,10 @@
 import styled from "styled-components";
 const Container = styled.div`
     height: 100vh;
-    background-color:#28bec6;
+    background-color:#346beb;
     display: flex; //Flex Container
     //This will make it up to down.
-    
+    //flex-direction:column;
 `;
 
 /**
@@ -18,22 +18,48 @@ const Leftside = styled.h1`
     text-align: center;
     margin-top: 20px;
     width:50%;
-    background-color: beige;
+    background-color: #f5426f;
 `;
-const Rightside = styled.div`
-    font-size: 100px;
-    color: blue;
-    text-align: center;
+const Rightside = styled.img`
+    // background-image: url("https://www.sportscasting.com/wp-content/uploads/2020/03/Boston-Red-Sox-bats-at-spring-training.jpg");
     margin-top: 20px;
     width:50%;
     height:88%;
-    background-color: #f57e42;
+    
+`;
+
+const Label = styled.label`
+    font-size:24px;
+    color: #f5ef42;
+    font-family: cursive;
+`
+const Input = styled.input`
+    font-size:24px;
+    position: relative;
+    left : 5%;
+`
+const WelcomeTitle = styled.h1`
+    color: #42f5e9;
+    font-weight: 800;
+    font-size: 40px;
+    margin-top: 100px;
 `;
 function LoginPage(){
     return (
     <Container>
-        <Leftside>Left Half Page</Leftside>
-        <Rightside>Right Half Page</Rightside>
+        <Leftside>
+            <WelcomeTitle>Welcome Back!</WelcomeTitle>
+           <div>
+            <Label>ID</Label>
+            <Input></Input>
+            </div>
+            <div>
+            <Label>Password</Label>
+            <Input></Input>
+            </div>
+        </Leftside>
+        <Rightside src="https://www.sportscasting.com/wp-content/uploads/2020/03/Boston-Red-Sox-bats-at-spring-training.jpg"></Rightside>
+        
     </Container>
     );
 }
