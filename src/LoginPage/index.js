d7ecfa88daab62dbf6ae9cd03394e84bc1d5cf17
손동찬 <charliesohn@sondongchan-ui-MacBookAir.local>
2022-03-02 09:10:28 +0900

    submitting

--- src/LoginPage/index.js
+++ src/LoginPage/index.js
@@ -16,15 +16,16 @@ const Leftside = styled.h1`
     font-size: 100px;
     color: blue;
     text-align: center;
-    margin-top: 20px;
+    margin-top: 0px;
     width:50%;
+    height:100%;
     background-color: #f5426f;
 `;
 const Rightside = styled.img`
     // background-image: url("https://www.sportscasting.com/wp-content/uploads/2020/03/Boston-Red-Sox-bats-at-spring-training.jpg");
-    margin-top: 20px;
+    margin-top: 0px;
     width:50%;
-    height:88%;
+    height:100%;
     
 `;
 
@@ -36,7 +37,7 @@ const Label = styled.label`
 const Input = styled.input`
     font-size:24px;
     position: relative;
-    left : 5%;
+    left : 3%;
 `
 const WelcomeTitle = styled.h1`
     color: #42f5e9;
@@ -44,6 +45,16 @@ const WelcomeTitle = styled.h1`
     font-size: 40px;
     margin-top: 100px;
 `;
+
+const Button = styled.button`
+    font-weight: 800;
+    color: #3503fc;
+    width:100px;
+    height:50px;
+    position: relative;
+    left: 3%;
+    background-color:#03fc90;
+`
 function LoginPage(){
     return (
     <Container>
@@ -57,6 +68,7 @@ function LoginPage(){
             <Label>Password</Label>
             <Input></Input>
             </div>
+            <Button>GO</Button>
         </Leftside>
         <Rightside src="https://www.sportscasting.com/wp-content/uploads/2020/03/Boston-Red-Sox-bats-at-spring-training.jpg"></Rightside>
         
