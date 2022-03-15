8320c33561d5fde68570e4c3f9221f3d537ce6cc
손동찬 <charliesohn@sondongchan-ui-MacBookAir.local>
2022-03-15 09:21:41 +0900

    submitting

--- src/MainPage/index.js
+++ src/MainPage/index.js
@@ -2,20 +2,29 @@ import styled from "styled-components";
 const Container = styled.div`
     height: 100vh;
     background-color:#346beb;
-    display: flex; //Flex Container
-    //This will make it up to down.
-    display: column;
+    display: flex; 
 `;
 const WelcomeTitle = styled.h1`
     color: #42f5e9;
     font-weight: 800;
     font-size: 40px;
-    margin-top: 100px;
+    margin-top: 150px;
+    text-align: right;
+`;
+
+const Introduce = styled.div`
+    color: #fcba03;
+    font-weight: 800;
+    font-size: 40px;
+    margin-top: 250px;
 `;
 function MainPage(){
-    <Container>
+    return(
+        <Container>
         <WelcomeTitle>Charlie</WelcomeTitle>
-    </Container>
+        <Introduce>Reading book, Remembering, do homework</Introduce>
+        </Container>
+    );
 }
 
 export default MainPage;
