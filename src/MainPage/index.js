d69219d1a6196a7b06f6dc9cc67229b87b1c4eda
손동찬 <charliesohn@sondongchan-ui-MacBookAir.local>
2022-03-15 09:50:39 +0900

    submitting

--- src/MainPage/index.js
+++ src/MainPage/index.js
@@ -3,13 +3,14 @@ const Container = styled.div`
     height: 100vh;
     background-color:#346beb;
     display: flex; 
+    display: column;
+    justify-content: center;
 `;
 const WelcomeTitle = styled.h1`
     color: #42f5e9;
     font-weight: 800;
     font-size: 40px;
     margin-top: 150px;
-    text-align: right;
 `;
 
 const Introduce = styled.div`
@@ -18,11 +19,19 @@ const Introduce = styled.div`
     font-size: 40px;
     margin-top: 250px;
 `;
+const Picture = styled.img`
+    margin-top: 350px;
+    width:10%;
+    height:10%;
+`
 function MainPage(){
     return(
         <Container>
         <WelcomeTitle>Charlie</WelcomeTitle>
-        <Introduce>Reading book, Remembering, do homework</Introduce>
+        <Introduce>Reading book, Watching baseball, do homework</Introduce>
+        <Picture src="https://www.oberlo.com/media/1612639204-image3.jpg?fit=max&fm=jpg&w=1824"></Picture>
+        <Picture src="https://img.global.news.samsung.com/global/wp-content/uploads/2015/07/Main_Thumb_0708.jpg"></Picture>
+        <Picture src="https://1en5vh48t4rqdnq1j3h9ihn4-wpengine.netdna-ssl.com/wp-content/uploads/2006/09/make-the-homework-hour-happy-hour-top-10-tips-860x420.jpg"></Picture>
         </Container>
     );
 }
